ca7f8069f512238053e83dd4c3439d64e173a4ef
Shinra Kusakabe <73338956+sasmithaK@users.noreply.github.com>
2025-02-19 23:35:20 +0530

    Transaction Feature : GET all transactions

---
+++ controllers/transactionController.js
@@ -0,0 +1,35 @@
+const Transaction = require('../models/Transaction');
+
+// @desc Get all transactions
+// @route GET/api/v1/transactions
+// @access Public
+exports.getTransactions = async (req, res, next) => {
+    try {
+        const transactions = await Transaction.find();
+
+        return res.status(200).json({
+            success: true,
+            count: transactions.length,
+            data: transactions
+        });
+    } catch (error) {
+        return res.send(500).json({
+            success: false,
+            error: 'Server Error'
+        });
+    }
+}
+
+// @desc Add transactions
+// @route POST/api/v1/transactions
+// @access Public
+exports.addTransactions = async (req, res, next) => {
+    res.send('POST transactions');
+}
+
+// @desc Delete transactions
+// @route DELETE/api/v1/transactions/:id
+// @access Public
+exports.deleteTransactions = async (req, res, next) => {
+    res.send('DELETE transactions');
+}
